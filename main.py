85f2edd0c857261db53c363b0fe7d2a778c5dec1
szymanskir <ryszard.szymanski@outlook.com>
2018-11-03 22:52:20 +0100

    Add size criterion options

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import click
 from src.utils import read_graph
-from src.algorithms import find_exact_mccis
+from src.algorithms import find_exact_mccis, SizeCriterion
 
 
 @click.command()
 @click.argument('graph_csv1', type=click.Path())
 @click.argument('graph_csv2', type=click.Path())
-def main(graph_csv1, graph_csv2):
+@click.option('--size_criterion', type=str)
+def main(graph_csv1, graph_csv2, size_criterion):
     graph1 = read_graph(graph_csv1)
     graph2 = read_graph(graph_csv2)
 
-    mcis = find_exact_mccis(graph1, graph2)
+
+    mcis = find_exact_mccis(graph1, graph2, size_criterion)
     print(mcis)
 
 
--- src/algorithms.py
+++ src/algorithms.py
@@ -1,60 +1,71 @@
 import networkx as nx
 from itertools import product
 from typing import NamedTuple
+from enum import Enum
 
 
-def find_exact_mccis(G1, G2):
-    return(find_mccis(G1, G2, find_exact_max_clique))
+def find_exact_mccis(G1, G2, size_criterion):
+    return(find_mccis(G1, G2, size_criterion, find_exact_max_clique))
 
 
-def find_mccis(G1, G2, max_clique_finder):
+def find_mccis(G1, G2, size_criterion, max_clique_finder):
+    # validate criterion
+    if not hasattr(SizeCriterion, size_criterion):
+        raise ValueError(f'{size_criterion} is not a valid size criterion.')
+
     H = modular_product(G1, G2)
-    mccis_isomorphism = max_clique_finder(H)
+    mccis_isomorphism = max_clique_finder(H, size_criterion)
 
     return(mccis_isomorphism)
 
 
-def find_exact_max_clique(graph):
+def find_exact_max_clique(graph, size_criterion):
     max_clique = _max_clique_backtracking(graph,
                                           Clique(tuple(), 0),
                                           list(graph.nodes()),
-                                          Clique(tuple(), 0))
+                                          Clique(tuple(), 0),
+                                          size_criterion)
     return max_clique
 
 
-def _max_clique_backtracking(H, clique, candidates, max_clique):
+def _max_clique_backtracking(H, clique, candidates, max_clique, size_criterion):
     if(clique.size > max_clique.size):
         max_clique = Clique(clique.vertices, clique.size)
 
     nodes_to_test = candidates.copy()
-    for v in nodes_to_test:
+    for candidate in nodes_to_test:
+        edge_size_increase = 0
         extends_clique = True
         meet_A_type = False
         for clique_vertex in clique.vertices:
-            if(not H.has_edge(v, clique_vertex)):
+            if not H.has_edge(candidate, clique_vertex):
                 extends_clique = False
                 break
-            edge_type = H.get_edge_data(v, clique_vertex, 'type')['type']
-            if(edge_type == 'A'):
+            edge_type = H.get_edge_data(candidate, clique_vertex, 'type')['type']
+            if edge_type == 'A':
+                edge_size_increase += 1
                 meet_A_type = True
 
-        if(not meet_A_type and clique.size > 0):
+        if not meet_A_type and clique.size > 0:
             continue
 
-        candidates.remove(v)
+        candidates.remove(candidate)
 
-        if(extends_clique):
-            max_clique = _max_clique_backtracking(H,
-                                                  expand_clique(clique, v, 1),
-                                                  candidates.copy(),
-                                                  max_clique)
+        if extends_clique:
+            max_clique = _max_clique_backtracking(
+                H,
+                expand_clique(clique, candidate, edge_size_increase, size_criterion),
+                candidates.copy(),
+                max_clique,
+                size_criterion
+            )
 
     return max_clique
 
 
 def _node_product(G, H):
-    for u, v in product(G, H):
-        yield ((u, v), dict())
+    for u, candidate in product(G, H):
+        yield ((u, candidate), dict())
 
 
 def modular_product(G, H):
@@ -65,19 +76,28 @@ def modular_product(G, H):
             if(x1 == x2 or y1 == y2):
                 continue
 
-            if(G.has_edge(x1, x2) and H.has_edge(y1, y2)):
+            if G.has_edge(x1, x2) and H.has_edge(y1, y2):
                 GH.add_edge((x1, y1), (x2, y2), type='A')
-            elif(not G.has_edge(x1, x2) and not H.has_edge(y1, y2)):
+            elif not G.has_edge(x1, x2) and not H.has_edge(y1, y2):
                 GH.add_edge((x1, y1), (x2, y2), type='B')
 
     return GH
 
 
-def expand_clique(clique, vertex, size_difference):
-    return Clique(clique.vertices + (vertex,),
-                  clique.size + size_difference)
-
-
 class Clique(NamedTuple):
     vertices: tuple
     size: int
+
+
+def expand_clique(clique, vertex, edge_increase, size_criterion):
+    if SizeCriterion[size_criterion] == SizeCriterion.Vertices:
+        return Clique(clique.vertices + (vertex,),
+                      clique.size + 1)
+    elif SizeCriterion[size_criterion] == SizeCriterion.VerticesAndEdges:
+        return Clique(clique.vertices + (vertex,),
+                      clique.size + 1 + edge_increase)
+
+
+class SizeCriterion(Enum):
+    Vertices = 1
+    VerticesAndEdges = 2

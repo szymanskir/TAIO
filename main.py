c0ea4924d61b641bf4d0ff6b167962041cb43055
szymanskir <ryszard.szymanski@outlook.com>
2018-11-04 12:43:31 +0100

    Add exact/approximation algorithm option to main command.

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from src.algorithms import find_exact_mccis
 
 
 @click.command()
-@click.argument('graph_csv1', type=click.Path())
-@click.argument('graph_csv2', type=click.Path())
+@click.argument('graph_csv1', type=click.Path(),
+                help="Path to the csv file containing data about the first graph.")
+@click.argument('graph_csv2', type=click.Path(),
+                help="Path to the csv file containing data about the second graph.")
+@click.argument('output_file', type=click.Path(),
+                help="Path to a file in which the result should be saved.")
 @click.option('--size_criterion', type=str)
-@click.argument('output_file', type=click.Path())
+@click.option('--exact/--approx', default=True)
 def main(graph_csv1, graph_csv2, size_criterion, output_file):
     graph1 = read_graph(graph_csv1)
     graph2 = read_graph(graph_csv2)
